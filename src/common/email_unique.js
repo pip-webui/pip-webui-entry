d9328395bfa8dfc5813b8f26ad47da9c13b6617b
Anastas Fonotov <anastasf@gmail.com>
2016-09-20 16:27:14 +0300

    fix emailUnique

--- src/common/email_unique.js
+++ src/common/email_unique.js
@@ -29,7 +29,7 @@
 
                         if (!newValue) ngModel.$setValidity('emailUnique', true);
 
-                        pipDataUser.signupValidate(email, 
+                        pipDataUser.signupValidate(newValue, 
                             function (data) {
                                 ngModel.$setValidity('emailUnique', true);
                             }, 
